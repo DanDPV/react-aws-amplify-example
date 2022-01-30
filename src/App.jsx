51ed4b36a6924ebd94783d53e8b2c99c0e09e0c3
Daniel De Paz <vvelasquez@applaudostudios.com>
2022-01-30 11:43:32 -0600

    feat: add like function

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
+import { useEffect, useState } from 'react';
 import Amplify, { API, graphqlOperation } from 'aws-amplify';
+import { IconButton, Paper } from '@material-ui/core';
 import awsConfig from './aws-exports';
 import { Authenticator, withAuthenticator } from '@aws-amplify/ui-react';
 import { listSongs } from './graphql/queries';
+import { updateSong } from './graphql/mutations';
 import '@aws-amplify/ui-react/styles.css';
 import './App.css';
-import { useEffect, useState } from 'react';
+import { Favorite, PlayArrow } from '@material-ui/icons';
 
 Amplify.configure(awsConfig);
 
@@ -24,6 +27,27 @@ function App() {
     }
   };
 
+  const addLike = async idx => {
+    try {
+      const song = songs[idx];
+      song.like = song.like + 1;
+      delete song.createdAt;
+      delete song.updatedAt;
+      const songData = await API.graphql({
+        query: updateSong,
+        authMode: 'AMAZON_COGNITO_USER_POOLS',
+        variables: {
+          input: song,
+        },
+      });
+      const songList = [...songs];
+      songList[idx] = songData.data?.updateSong;
+      setSongs(songList);
+    } catch (error) {
+      console.error('Error on adding like to song', error);
+    }
+  };
+
   useEffect(() => {
     fetchSongs();
   }, []);
@@ -36,6 +60,31 @@ function App() {
             <div className="App">
               <p>Hey {user.username}, welcome to my channel, with auth!</p>
               <button onClick={signOut}>Sign out</button>
+              <div className="song-list">
+                {songs.map((song, idx) => (
+                  <Paper variant="outlined" elevation={2} key={song.title}>
+                    <div className="song-card">
+                      <IconButton aria-label="play">
+                        <PlayArrow />
+                      </IconButton>
+                      <div>
+                        <div className="song-title">{song.title}</div>
+                        <div className="song-owner">{song.owner}</div>
+                      </div>
+                      <div>
+                        <IconButton
+                          aria-label="like"
+                          onClick={() => addLike(idx)}
+                        >
+                          <Favorite />
+                        </IconButton>
+                        {song.like}
+                      </div>
+                      <div className="song-description">{song.description}</div>
+                    </div>
+                  </Paper>
+                ))}
+              </div>
             </div>
           )}
         </Authenticator>
