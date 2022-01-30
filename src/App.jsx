9d16da90ae672f4a8029eab99756ddc38f06fe8e
Daniel De Paz <vvelasquez@applaudostudios.com>
2022-01-29 21:21:18 -0600

    feat: fetch songs items in homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,33 @@
-import Amplify from 'aws-amplify';
+import Amplify, { API, graphqlOperation } from 'aws-amplify';
 import awsConfig from './aws-exports';
 import { Authenticator, withAuthenticator } from '@aws-amplify/ui-react';
+import { listSongs } from './graphql/queries';
 import '@aws-amplify/ui-react/styles.css';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 Amplify.configure(awsConfig);
 
 function App() {
+  const [songs, setSongs] = useState([]);
+  const fetchSongs = async () => {
+    try {
+      const songData = await API.graphql({
+        query: listSongs,
+        authMode: 'AMAZON_COGNITO_USER_POOLS',
+      });
+      const songList = songData.data?.listSongs.items;
+      console.log(songList);
+      setSongs(songList);
+    } catch (error) {
+      console.error('Error on fetching songs', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchSongs();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
